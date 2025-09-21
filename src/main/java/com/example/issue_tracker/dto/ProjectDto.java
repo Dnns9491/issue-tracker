a381fa74d3ae0ffae743a66f9143c39778be8e87
Dennis Sasi <dennis_1994@hotmail.co.uk>
2025-09-22 00:07:07 +0100

    fixing lintin

--- src/main/java/com/example/issue_tracker/dto/ProjectDto.java
+++ src/main/java/com/example/issue_tracker/dto/ProjectDto.java
@@ -2,5 +2,5 @@ package com.example.issue_tracker.dto;
 
 import java.time.Instant;
 
-public record ProjectDto (Long id, String key, String name, Instant createdAt) {
+public record ProjectDto(Long id, String key, String name, Instant createdAt) {
 }
